9dd768a11befb695e2c94e30ff732f8f3cab9bb2
dgroch <daniel.j.groch@gmail.com>
2016-09-28 15:53:59 +1000

    Update file paths in init.sh

--- base/scripts/init.sh
+++ base/scripts/init.sh
@@ -1,5 +1,5 @@
 set -e
 
-bash $METEORD_DIR/lib/install_node.sh
-bash $METEORD_DIR/lib/install_phantomjs.sh
-bash $METEORD_DIR/lib/cleanup.sh
+bash $METEORD_DIR/base/scripts/lib/install_node.sh
+bash $METEORD_DIR/base/scripts/lib/install_phantomjs.sh
+bash $METEORD_DIR/base/scripts/lib/cleanup.sh
